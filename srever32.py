d3191d9e8d6ada218007f1cbb41a641065677cbf
Cisco <40459710+cisco-root@users.noreply.github.com>
2018-06-24 14:39:15 +0430

    Add files via upload

--- srever32.py
+++ srever32.py
@@ -1,7 +1,21 @@
 import whois
-
+import os
+os.system('cls')
 site = input("site: ")
 
 s = whois.whois(site)
+
 print(s)
+import urllib.request
+import time
+time.sleep(6)
+print('''
+ _____ _____  _____  _____ ____  
+ / ____|_   _|/ ____|/ ____/ __ \ 
+| |      | | | (___ | |   | |  | |
+| |      | |  \___ \| |   | |  | |
+| |____ _| |_ ____) | |___| |__| |
+ \_____|_____|_____/ \_____\____/ 
+''')
+print ("by cisco")
 input("\n\n Press enter to exit")
